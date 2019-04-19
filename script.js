ae1ad8f8159f0a8d2275ddda04ef1de9065e40af
Kailin Ke <kake@s.sfusd.edu>
2019-04-19 01:00:30 +0000

    added ajax

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-var authorizationToken = "ME65u51_soQ78ULazWrZ_muetikm6UYwm9KrAVzoZS7XcBbu-vd3VnSa032uYWWL";
-debugger;
-$.ajax({
- method: "GET",
- url: "https://api.genius.com/annotations/10225840",
- beforeSend: function(xhr) {
-     xhr.setRequestHeader('Authorization', 'Bearer ' + authorizationToken);
- },
- success: function(msg) {
-     console.log(msg);
- },
- error: function(jqXHR, textStatus, errorThrown) {
-     console.log(textStatus);
- }
+var key = "apikey=5ca0449b957dbc8957a995943e4c93bd";
+$("#button1").click(function(){
+ let input=$("#inputName").val();
+ $.ajax({
+  method: "GET",
+  url: `https://api.musixmatch.com/ws/1.1/track.search?q_track=${input}&${key}`,
+ 
+  success: function(response) {
+     let trackId=response.body.track_list[0].track.track_id;
+     $.ajax({
+      method: "GET",
+      url: `https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${trackId}&${key}`,
+     
+      success: function(page) {
+         $("body").append(page.body.lyrics.lyrics_body);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+          console.log(textStatus);
+      }
+     });
+  },
+  error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+  }
+ });
+ 
 });
+
+
+
