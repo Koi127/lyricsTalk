100c3e068090a471c8ad8be7743ed18e5948fa3e
Kailin Ke <kake@s.sfusd.edu>
2019-04-26 00:31:24 +0000

    fixed some cross origin error

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
-var key = "apikey=5ca0449b957dbc8957a995943e4c93bd";
-$("#button1").click(function(){
- let input=$("#inputName").val();
- $.ajax({
-  method: "GET",
-  url: `https://api.musixmatch.com/ws/1.1/track.search?q_track=${input}&${key}`,
- 
-  success: function(response) {
-     let trackId=response.body.track_list[0].track.track_id;
-     $.ajax({
-      method: "GET",
-      url: `https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${trackId}&${key}`,
-     
-      success: function(page) {
-         $("body").append(page.body.lyrics.lyrics_body);
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-          console.log(textStatus);
-      }
-     });
-  },
-  error: function(jqXHR, textStatus, errorThrown) {
-      console.log(textStatus);
-  }
+/* global $ */
+$( document ).ready(function() {
+ var key = "5ca0449b957dbc8957a995943e4c93bd";
+ $("#button1").click(function(e){
+  e.preventDefault();
+  let input=$("#inputName").val();
+  $.ajax({
+   type: "GET",
+   url: "https://api.musixmatch.com/ws/1.1/track.search",
+   data: {
+    apikey: key,
+    q_track: input,
+    format:"jsonp",
+    callback:"jsonp_callback"
+   },
+   dataType: "jsonp",
+   jsonpCallback: 'jsonp_callback',
+   contentType: 'application/json',
+   success: function(response) {
+    console.log("pass");
+      let trackId=response.body.track_list[0].track.track_id;
+      $.ajax({
+       method: "GET",
+       url: "https://api.musixmatch.com/ws/1.1/track.lyrics.get?",
+       data: {
+        apikey: key,
+        track_id: trackId,
+        format:"jsonp",
+        callback:"jsonp_callback"
+       },
+       dataType: "jsonp",
+       jsonpCallback: 'jsonp_callback',
+       contentType: 'application/json',
+       success: function(page) {
+        console.log("hi");
+          $("body").append(page.body.lyrics.lyrics_body);
+       },
+       error: function(jqXHR, textStatus, errorThrown) {
+           console.log(textStatus);
+       }
+      });
+   },
+   error: function(jqXHR, textStatus, errorThrown) {
+       console.log(textStatus);
+   }
+  });
+  
  });
- 
 });
-
 
 
